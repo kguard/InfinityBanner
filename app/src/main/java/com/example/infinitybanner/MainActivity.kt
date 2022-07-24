4bbcaa8d1afcb248018bc7f60b466ba2fbbe26b6
kguard <kkh9331@naver.com>
2022-07-25 03:08:39 +0900

    Infinity Banner 추가 (자동 스크롤 x )

--- app/src/main/java/com/example/infinitybanner/MainActivity.kt
+++ app/src/main/java/com/example/infinitybanner/MainActivity.kt
@@ -2,16 +2,20 @@ package com.example.infinitybanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.viewpager2.widget.ViewPager2
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val viewPager=findViewById<ViewPager2>(R.id.ViewPager)
-        viewPager.adapter=ViewPagerAdapter(getList())
+        viewPager.adapter=ViewPagerAdapter(getList(),viewPager)
         viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        val current=Int.MAX_VALUE/2- kotlin.math.ceil(getList().size.toDouble() / 2).toInt()
+        viewPager.setCurrentItem(current,false)
     }
     private fun getList():ArrayList<Int>{
         return arrayListOf<Int>(R.drawable.orangemushroom,R.drawable.pinkbean,R.drawable.rockspirit,R.drawable.yeti,R.drawable.slime)
--- app/src/main/java/com/example/infinitybanner/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/infinitybanner/ViewPagerAdapter.kt
@@ -5,18 +5,32 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 
-class ViewPagerAdapter(val item: ArrayList<Int>):RecyclerView.Adapter<ViewPagerAdapter.ViewPagerViewHolder>() {
+class ViewPagerAdapter(val item: ArrayList<Int>,val viewPager2:ViewPager2):RecyclerView.Adapter<ViewPagerAdapter.ViewPagerViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerViewHolder {
         val inflater=LayoutInflater.from(parent.context).inflate(R.layout.item_holder,parent,false)
         return ViewPagerViewHolder(inflater)
     }
     override fun onBindViewHolder(holder: ViewPagerViewHolder, position: Int) {
-        holder.setImage(item[position])
+        holder.setImage(item[position%item.size])//Int 최대 값으로 무한
+       /* holder.setImage(item[position])
+        if(position==item.size-2){
+            viewPager2.post(runnable())
+        }*/ //Runnable 로 무한: 왼쪽으로 x
     }
+    /*inner class runnable:Runnable{
+        override fun run() {
+            item.addAll(item)
+            notifyDataSetChanged()
+        }
+
+    }*/ //Runnable 로 무한: 왼쪽으로 x
     override fun getItemCount(): Int {
-        return item.size
+        return Int.MAX_VALUE //Int 최대 값으로 무한
+        //return item.size //Runnable 로 무한: 왼쪽으로 x
+
     }
     inner class ViewPagerViewHolder(view: View):RecyclerView.ViewHolder(view){
         private val image=view.findViewById<ImageView>(R.id.imageView)
